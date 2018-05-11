1aa658332e566d7d0f7664053d7be4bda2e3c072
sbk620 <sbk620@naver.com>
2018-05-11 18:41:33 +0900

    DateUtil.java주석 추가

--- KH10/src/el/DateUtil.java
+++ KH10/src/el/DateUtil.java
@@ -7,7 +7,7 @@ public class DateUtil {
 	private static SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 	
 	public static String format(Date date) {
-		return formatter.format(date);
+		return formatter.format(date);//dateutil
 	} 
 
 }
